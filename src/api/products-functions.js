e4701616bca2223adcc06de225d7eeb9ae3e5c23
Eti <etibr@gmail.com>
2026-02-09 08:39:06 +0200

    added /api/ in api

--- src/api/products-functions.js
+++ src/api/products-functions.js
@@ -1,6 +1,6 @@
 export const handleProducts = async () => {
   // const response = await fetch("https://fakestoreapi.com/products");
-  const response = await fetch("http://localhost:3000/products");
+  const response = await fetch("http://localhost:3000/api/products");
   const data = await response.json();
 
   return data.map((product) => {
@@ -10,7 +10,7 @@ export const handleProducts = async () => {
 
 export const fetchSingleProduct = async (id) => {
   // const response = await fetch(`https://fakestoreapi.com/products/${id}`);
-  const response = await fetch(`http://localhost:3000/products/id/${id}`);
+  const response = await fetch(`http://localhost:3000/api/products/id/${id}`);
 
   if (!response.ok) throw new Error("Product not found");
   return response.json();
