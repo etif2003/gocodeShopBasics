bf904f7e7376bf23128e7a4d9ec1ab8efbc47f1b
etif2003 <etib2003@gmail.com>
2026-02-10 11:39:40 +0200

    Update NavSection.jsx

--- src/components/NavSection.jsx
+++ src/components/NavSection.jsx
@@ -1,18 +1,29 @@
 import RangeSlider from "./RangeSlider";
 import { SortSection } from "./SortSection";
 import CartDrawer from "./CartDrawer";
+import { Link } from "react-router";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import ManageAccountsIcon from "@mui/icons-material/ManageAccounts";
+
 
 export const NavSection = () => {
   return (
     <nav className="product-filter">
-      <h1>Products</h1>
+      <h1 className="nav-title">Products</h1>
 
-      <div className="filter-section">
-        <SortSection />
-        <RangeSlider />
-      </div>
-      <div>
+      <div className="nav-right">
+        <div className="filters">
+          <SortSection />
+          <RangeSlider />
+        </div>
+
         <CartDrawer />
+        <Tooltip title="Admin">
+          <IconButton component={Link} to="/manageProducts" aria-label="Admin">
+            <ManageAccountsIcon/>
+          </IconButton>
+        </Tooltip>
       </div>
     </nav>
   );
